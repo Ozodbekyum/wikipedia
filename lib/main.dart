7337e19ede97e9f5c65036fb64c777e1dad55042
Ozodbekyum <hello7ozod@gamil.com>
2023-01-21 14:49:44 +0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:wikipedia/model/music_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WikipediaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WikipediaApp extends StatelessWidget {
+  const WikipediaApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SearchPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SearchPageState extends State<SearchPage> {
+  static List<MusicModel> main_musics_list = [
+    MusicModel('Love me(Bluefin Remix)', 'Zyrex'),
+    MusicModel('Comfort Chain', 'Instupendo'),
+    MusicModel('Play date', 'Melanie Martinez'),
+    MusicModel('Dandelions', 'Ruth B.'),
+    MusicModel('Bad Habit', 'Steve Lacy'),
+    MusicModel('METAMORPHOSIS', 'INTERWORLD'),
+    MusicModel('Rises the moon', 'Liana Flores'),
+    MusicModel('No Idea', 'Don Toliver'),
+    MusicModel('InEssence', 'KA\$TRO'),
+    MusicModel('Another Love', 'Tom Odell'),
+    MusicModel('Falling', 'unkown'),
+    MusicModel('amend', 'j^p^n'),
+    MusicModel('Yummy', 'Justin Bieber'),
+    MusicModel('Starboy', 'The Weekend'),
+    MusicModel('Moonlight', 'XXXTENTACION'),
+  ];
 
-  void _incrementCounter() {
+  List<MusicModel> display_list = List.from(main_musics_list);
+
+  void updateList(String value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      display_list = main_musics_list
+          .where((element) =>
+              element.music_title!.toLowerCase().contains(value.toLowerCase()))
+          .toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color(0xFF1f1545),
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Color(0xFF1f1545),
+        elevation: 0.0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             const Text(
-              'You have pushed the button this many times:',
+              'Search for a Music',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: (value) => updateList(value),
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xff302360),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'eg: Moonlight',
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: display_list.length,
+                itemBuilder: (context, index) => ListTile(
+                  onTap: () {},
+                  contentPadding: EdgeInsets.all(8.0),
+                  title: Text(
+                    display_list[index].music_title!,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    display_list[index].music_singer!,
+                    style: TextStyle(
+                      color: Colors.white60,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/model/music_model.dart
@@ -0,0 +1,6 @@
+class MusicModel {
+  String? music_singer;
+  String? music_title;
+
+  MusicModel(this.music_title, this.music_singer) {}
+}
